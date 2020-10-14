c26ab33cb93c59696ac66a15a0cd2740ce1aec2c
empeje <11813607+empeje@users.noreply.github.com>
2020-10-15 03:15:31 +0700

    feat(chat): implement receive chat

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,2 +1,36 @@
-import 'bootstrap/dist/css/bootstrap.css';
-console.log("GILE")
+import './common'
+
+const urlParams = new URLSearchParams(window.location.search);
+
+const username = urlParams.get("username")
+const port = urlParams.get("port")
+
+const usernameElement = document.getElementById("username")
+const portElement = document.getElementById("port")
+const connectionElement = document.getElementById("connection-status")
+const chatboxElement = document.getElementById("chatbox")
+
+usernameElement.innerText = username
+portElement.innerText = port
+
+const webSocket = new WebSocket(`ws://localhost:${port}`);
+
+webSocket.onopen = () => {
+  connectionElement.innerText = "Connected"
+}
+
+webSocket.onerror = () => {
+  connectionElement.innerText = "Error"
+}
+
+webSocket.onclose = () => {
+  connectionElement.innerText = "Disconnected"
+}
+
+webSocket.onmessage = (event) => {
+  const newMessage = document.createElement('p')
+  const {user, content} = JSON.parse(event.data)
+  newMessage.innerText = `${user} said ${content}`
+  chatboxElement.appendChild(newMessage)
+}
+
