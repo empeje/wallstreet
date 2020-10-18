3d2ae325a7c9a9444f5abeacebc84728c9350d0e
empeje <11813607+empeje@users.noreply.github.com>
2020-10-19 00:20:00 +0700

    feat(chat): install fontawesome

--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/main.css';
+import 'font-awesome/css/font-awesome.css';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
