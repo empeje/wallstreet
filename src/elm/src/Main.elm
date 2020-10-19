e37d2d247a96b2cf1e6127fe03125156c5167b1a
empeje <11813607+empeje@users.noreply.github.com>
2020-10-20 00:29:04 +0700

    feat(elm-chat): login UI

--- src/elm/src/Main.elm
+++ src/elm/src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, form, h5, input, label, section, text)
+import Html.Attributes exposing (action, attribute, class, for, id, method, name, placeholder, type_)
 
 
 type alias Model =
@@ -31,10 +31,37 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick Increment ] [ text "+1" ]
-        , div [] [ text <| String.fromInt model.count ]
-        , button [ onClick Decrement ] [ text "-1" ]
+    section [ class "login" ]
+        [ div [ class "container" ]
+            [ div [ class "alert alert-danger", id "flash", attribute "role" "alert", attribute "style" "display: none" ]
+                []
+            , div [ class "row" ]
+                [ div [ class "col-sm-9 col-md-7 col-lg-5 mx-auto" ]
+                    [ div [ class "card card-signin my-5" ]
+                        [ div [ class "card-body" ]
+                            [ h5 [ class "card-title text-center" ]
+                                [ text "Sign In" ]
+                            , form [ action "chat.html", class "form-signin", id "signin-form", method "GET" ]
+                                [ div [ class "form-label-group" ]
+                                    [ input [ attribute "autofocus" "", class "form-control", id "inputUsername", name "username", placeholder "Username", attribute "required" "", type_ "text" ]
+                                        []
+                                    , label [ for "inputUsername" ]
+                                        [ text "Username" ]
+                                    ]
+                                , div [ class "form-label-group" ]
+                                    [ input [ class "form-control", id "inputPort", name "port", placeholder "Port", attribute "required" "", type_ "text" ]
+                                        []
+                                    , label [ for "inputPort" ]
+                                        [ text "Port" ]
+                                    ]
+                                , button [ class "btn btn-lg btn-primary btn-block text-uppercase", type_ "submit" ]
+                                    [ text "Sign in" ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
         ]
 
 
